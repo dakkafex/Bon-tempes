de59391dc5be3e3d3c0b0d118635efd6f5905a5c
Bas <bas@tempestmedia.nl>
2017-11-29 13:11:13 +0100

    initial file commit

---
+++ ZureRoom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZureRoom.MvcApplication" Language="C#" %>
---
+++ ZureRoom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZureRoom.Startup))]
+namespace ZureRoom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
