494397b8ca0656701c3b51d248a88e999191cd68
Unknown <flonsknor@gmail.com>
2017-12-01 11:33:44 +0100

    Contact model
    model gemaakt

---
+++ ZureRoom/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZureRoom.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+        public string Email { get; set; }
+        public string TelefoonNmr { get; set; }
+        public string Bericht { get; set; }
+    }
+}
