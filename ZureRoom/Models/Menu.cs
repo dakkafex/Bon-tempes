0f07491182fec8fee30b91fd81d7c879b81ce753
Unknown <stefano-ras@live.nl>
2017-12-01 10:56:35 +0100

    Menu
    Added menu page

---
+++ ZureRoom/Models/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZureRoom.Models
+{
+    public class Menu
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+        public int Ordering { get; set; }
+        public string Beschrijving { get; set; }
+        public bool Noten { get; set; }
+        public bool Schelpdieren { get; set; }
+        public bool Soya { get; set; }
+        public bool Eieren { get; set; }
+        public bool Melk { get; set; }
+    }
+}
