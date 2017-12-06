9e0ba6027fa00495e2839ea91f9db62422740ca6
Unknown <stefano-ras@live.nl>
2017-12-06 14:29:34 +0100

    Newsletter
    Nieuwsbrief model aangemaakt.

---
+++ ZureRoom/Models/NewsLetter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZureRoom.Models
+{
+    public class Newsletter
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
