96bc035463bbe1d8e3e57dba3368476c090df191
Unknown <stefano-ras@live.nl>
2017-12-06 14:14:08 +0100

    Reservation
    Reservation pagina aangemaakt.

---
+++ ZureRoom/Models/Reservation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZureRoom.Models
+{
+    public class Reservation
+    {
+        public int ID { get; set; }
+        public int MenuID { get; set; }
+        public int Count { get; set; }
+        public int Size { get; set; }
+    }
+}
