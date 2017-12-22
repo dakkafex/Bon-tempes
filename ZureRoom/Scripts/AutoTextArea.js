43ff4a2412561ba51fa97786fdf4728d0fd4bd8f
Unknown <flonsknor@gmail.com>
2017-12-22 10:05:41 +0100

    Auto vergroten TextArea

---
+++ ZureRoom/Scripts/AutoTextArea.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('textarea').each(function () {
+        this.setAttribute('style', 'height:' + (this.scrollHeight + 5) + 'px;overflow-y:hidden;');
+    }).on('input', function () {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+});
