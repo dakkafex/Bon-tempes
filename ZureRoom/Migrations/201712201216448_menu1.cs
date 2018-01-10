6c04f86b6bb6b950526a64ffb0f10b59ff799964
Unknown <stefano-ras@live.nl>
2018-01-10 13:38:46 +0100

    Rescaffold menu/reservation

--- ZureRoom/Migrations/201712201216448_menu1.cs
+++ ZureRoom/Migrations/201712201216448_menu1.cs
@@ -7,10 +7,25 @@ namespace ZureRoom.Migrations
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.Reservations",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        MenuID = c.Int(nullable: false),
+                        Name = c.String(),
+                        Email = c.String(),
+                        Phone = c.String(),
+                        Size = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ID);
+            
         }
         
         public override void Down()
         {
+            DropTable("dbo.Reservations");
         }
     }
 }
