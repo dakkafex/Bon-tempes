94e14547c8de5837f278728a527bde1cfa6d9e73
Unknown <flonsknor@gmail.com>
2018-02-02 12:25:21 +0100

    Revert "Merge remote-tracking branch 'origin/Development' into Development"
    This reverts commit 293d04cee622253397040ad30613a0d667743998.

--- ZureRoom/Controllers/FrontendController.cs
+++ ZureRoom/Controllers/FrontendController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ZureRoom.Models;
 
 namespace ZureRoom.Controllers
 {
@@ -16,9 +15,7 @@ namespace ZureRoom.Controllers
         // GET: Frontend
         public ActionResult Home()
         {
-            var menu = from a in db.Menus
-                       select a;
-            return View(menu.ToList());
+            return View();
         }
 
         public ActionResult Reserveren()
