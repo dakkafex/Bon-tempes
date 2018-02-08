88a7059ec9af4a4d530b032b4751e0f66187b474
Unknown <stefano-ras@live.nl>
2018-02-08 11:38:57 +0100

    Opening Times

--- ZureRoom/Controllers/FrontendController.cs
+++ ZureRoom/Controllers/FrontendController.cs
@@ -9,6 +9,9 @@ namespace ZureRoom.Controllers
 {
     public class FrontendController : Controller
     {
+        //Create view maken
+        //Foreach Menu 
+
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Frontend
         public ActionResult Home()
@@ -16,6 +19,33 @@ namespace ZureRoom.Controllers
             var menu = from a in db.Menus
                        select a;
             return View(menu.ToList());
+        }
+
+        public ActionResult Reserveren()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Reserveren([Bind(Include ="Name,Email,Phone,Size,Message,Date")]Reservation reservation )
+        {
+            reservation.Price = 0;
+            reservation.Amount = 0;
+            reservation.MenuName = "0";
+
+            if (ModelState.IsValid)
+            {
+                db.Reservations.Add(reservation);
+                db.SaveChanges();
+                return RedirectToAction("Home");
+            }
+            return View();
+        }
+
+        public ActionResult Over()
+        {
+            return View();
         }
     }
 }
